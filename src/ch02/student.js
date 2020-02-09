7501da72f0eed8b6aca5fc5a99483fc7c42a2e9e
Solovev Pavel <p.solovev@elewise.com>
2020-02-09 15:08:17 +0400

    differences oop

---
+++ src/ch02/student.js
@@ -0,0 +1,114 @@
+class Person {
+  constructor(firsname, lastname, ssn) {
+    this._firstname = firsname;
+    this._lastname = lastname;
+    this._ssn = ssn;
+    this._address = null;
+    this._birthYear = null;
+  }
+
+  get ssn() {
+    return this._ssn;
+  }
+
+  get firsname() {
+    return this._firstname;
+  }
+
+  get lastname() {
+    return this._lastname;
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  get birthYear() {
+    return this._birthYear;
+  }
+
+  set birthYear(year) {
+    this._birthYear = year;
+  }
+
+  set address(addr) {
+    this._address = addr;
+  }
+
+  toString() {
+    return `Person(${this._firstname}, ${this._lastname})`;
+  }
+
+  peopleInSameCountry(friends) {
+    const result = [];
+    for(let idx in friends) {
+      const friend = friends[idx];
+      if(this.address.country === friend.address.country) {
+        result.push(friend);
+      }
+    }
+
+    return result;
+  }
+}
+
+class Student extends Person {
+  constructor(firsname, lastname, ssn, school) {
+    super(firsname, lastname, ssn);
+    this._school = school;
+  }
+
+  get school() {
+    return this._school;
+  }
+
+  studentsInSameCountryAndSchool(friends) {
+    const closeFriends = super.peopleInSameCountry(friends);
+    const result=[];
+
+    for (let idx in closeFriends) {
+      const friend = closeFriends[idx];
+      if (friend.school === this.school){
+        result.push(friend);
+      }
+    }
+
+    return result;
+  }
+}
+
+class Address {
+  constructor(country) {
+    this._country = country;
+  }
+
+  get country() {
+    return this._country;
+  }
+}
+
+const curry = new Student('Haskell', 'Curry', '111-11-1111', 'Penn State');
+curry.address = new Address('US');
+
+const turing = new Student('Alan', 'Turing', '222-22-2222', 'Princeton');
+turing.address = new Address('England');
+
+const church = new Student('Alonzo', 'Crunch', '333-33-3333', 'Princeton');
+church.address = new Address('US');
+
+const kleene = new Student('Stephen', 'Kleene', '444-44-4444', 'Princeton');
+kleene.address = new Address('US');
+
+church.studentsInSameCountryAndSchool([curry, turing, kleene]);
+console.log('church.studentsInSameCountryAndSchool([curry, turing, kleene])', church.studentsInSameCountryAndSchool([curry, turing, kleene]));
+
+function selector(country, school) {
+  return function(student) {
+    return student.address.country === country && student.school === school;
+  }
+}
+
+const findStudentBy = function(friends, selector) {
+  return friends.filter(selector);
+}
+console.log(" findStudentBy([curry, turing, kleene], selector('US', 'Princeton')):", findStudentBy([curry, turing, kleene], selector('US', 'Princeton')));
